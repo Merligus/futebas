bab10e9acb8e3e3c845ca823b5858b3e1d27f89b
Merligus <gustavomerli18@gmail.com>
2021-06-15 00:56:30 -0300

    C++

---
+++ OJogo/Source/OJogo/OJogo.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class OJogo : ModuleRules
+{
+	public OJogo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+	}
+}
---
+++ OJogo/Source/OJogo/OJogoGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OJogoGameMode.h"
+#include "OJogoCharacter.h"
+
+AOJogoGameMode::AOJogoGameMode()
+{
+	// Set default pawn class to our character
+	// DefaultPawnClass = AOJogoCharacter::StaticClass();	
+}
